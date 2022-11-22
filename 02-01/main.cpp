d9c2dcab045ef760d90e27c182d51f77c55f2380
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-11-22 16:18:43 +0900

    これでいいのかな

--- 02-01/main.cpp
+++ 02-01/main.cpp
@@ -1,19 +1,67 @@
 #include <list>
+#include <stdio.h>
 
 using namespace std;	//これでstd::を省略可能
 
 int main(void) {
 
-	//list<int> lst(33,4);
-	list<int> list{ 1,4,5,6,9 };//初期設定リスト
-
-	for (std::list<int>::iterator itr=list.begin(); itr != list.end(); ++itr)
+	//コンテナ名
+	list<const char*> yamanotesenn =
 	{
-		if (*itr==4)
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata"
+	};
+
+	//1970年
+	printf("-1970-\n");
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+	//改行
+	printf("\n");
+
+	//2019年
+	//1971年、田端の前に、日暮里の後に西日暮里が追加
+	printf("-2019-\n");
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); ++itr)
+	{
+		if (strcmp(*itr, "Tabata") == 0)
 		{
-			itr = list.insert(itr, 0);
-			++itr;		//重要
+			itr = yamanotesenn.insert(itr, "Nishi-Nippori");
+			itr++;
+			//brackでも可
 		}
 	}
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+	//改行
+	printf("\n");
+
+	//2022年
+	printf("-2022-\n");
+	//2020年に高輪ゲートウェイが追加される
+	yamanotesenn.push_back("Takanawa-Gateway");		
+	
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+	
+	//改行
+	printf("\n");
+
 	return 0;
 }
--- 02-01/memo.cpp
+++ 02-01/memo.cpp
@@ -80,4 +80,26 @@ int main(void) {
 	{
 		lst.push_back(i);
 	}
+
+	std::list<const char*>
+
+	#include <list>
+
+using namespace std;	//これでstd::を省略可能
+
+int main(void) {
+
+	//list<int> lst(33,4);
+	list<int> list{ 1,4,5,6,9 };//初期設定リスト
+
+	for (std::list<int>::iterator itr=list.begin(); itr != list.end(); ++itr)
+	{
+		if (*itr==4)
+		{
+			itr = list.insert(itr, 0);
+			++itr;		//重要
+		}
+	}
+	return 0;
+}
 */
