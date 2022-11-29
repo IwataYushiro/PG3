ddc1c79f00b21e16ef596bc4d7624ececb28c3b2
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-11-29 13:46:17 +0900

    2-1の内容を評価課題に一旦コピー

---
+++ 評価課題1/main.cpp
@@ -0,0 +1,67 @@
+#include <list>
+#include <stdio.h>
+
+using namespace std;	//これでstd::を省略可能
+
+int main(void) {
+
+	//コンテナ名
+	list<const char*> yamanotesenn =
+	{
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata"
+	};
+
+	//1970年
+	printf("-1970-\n");
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+	//改行
+	printf("\n");
+
+	//2019年
+	//1971年、田端の前に、日暮里の後に西日暮里が追加
+	printf("-2019-\n");
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); ++itr)
+	{
+		if (strcmp(*itr, "Tabata") == 0)
+		{
+			itr = yamanotesenn.insert(itr, "Nishi-Nippori");
+			itr++;
+			//brackでも可
+		}
+	}
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+	//改行
+	printf("\n");
+
+	//2022年
+	printf("-2022-\n");
+	//2020年に高輪ゲートウェイが追加される
+	yamanotesenn.push_back("Takanawa-Gateway");
+
+	for (auto itr = yamanotesenn.begin();
+		itr != yamanotesenn.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+
+	//改行
+	printf("\n");
+
+	return 0;
+}
